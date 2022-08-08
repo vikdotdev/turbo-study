30b0f4b89005779cb6daed1505da28971438f2ff
Viktor Habchak <vikdotdev@gmail.com>
2022-08-08 16:32:22 +0300

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+#
+system("bin/rails db:fixtures:load")
